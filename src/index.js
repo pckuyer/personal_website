205f0479f727f22d902dbb88c97e993409290619
Paul Kuyer <paul.kuyer@tuta.io>
2023-03-19 18:56:10 +0000

    menubar with active link and smooth scroll feature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import './style.css';
 
-document.addEventListener("load", function () {
+const sections = document.querySelectorAll("section")
+const links = document.querySelectorAll("nav ul li a") 
+document.addEventListener("scroll", function () {
 
-	console.log("test")
-
+	sections.forEach(section => {
+		if (pageYOffset >= (section.offsetTop - (section.offsetHeight / 2.5))) {
+			links.forEach(link => {
+				link.classList.remove("active")
+				if (link.innerHTML.toLowerCase() === section.id){ //.getAttribute("href") would get #color
+					link.classList.add("active")
+				}
+			})
+		}
+	})
 })
 
+links.forEach(link => {
+	link.addEventListener("click", function () {
+		console.log(link.innerHTML.toLowerCase()) //.getAttribute("href") would get #color
+	})
+})
