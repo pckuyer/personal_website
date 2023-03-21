78399efe032db7eb89cb8b144e4076234dcb2be4
Paul Kuyer <paul.kuyer@tuta.io>
2023-03-21 10:26:37 +0000

    created effect to shrink div on scroll

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import './style.css';
 
+// show which section is viewed in menu bar with active link
+
+
 const sections = document.querySelectorAll("section")
 const links = document.querySelectorAll("nav ul li a") 
+
+const green = document.querySelector(".green")
+
+
 document.addEventListener("scroll", function () {
 
 	sections.forEach(section => {
-		if (pageYOffset >= (section.offsetTop - (section.offsetHeight / 2.5))) {
+		if (pageYOffset >= (section.offsetTop - (section.offsetHeight / 2))) {
 			links.forEach(link => {
 				link.classList.remove("active")
 				if (link.innerHTML.toLowerCase() === section.id){ //.getAttribute("href") would get #color
@@ -14,10 +21,19 @@ document.addEventListener("scroll", function () {
 			})
 		}
 	})
+
+
+
+	if (pageYOffset > green.offsetTop) {
+		const x = 1 - (pageYOffset - green.offsetTop) * 0.0005 
+		green.style.transform = `scale(${x})`
+		// const width = screen.width - 0.5 * (pageYOffset - green.offsetTop)
+		// green.style.width = width + "px"
+	} else {
+		green.style.transform = "none"
+	}
+
+
 })
 
-links.forEach(link => {
-	link.addEventListener("click", function () {
-		console.log(link.innerHTML.toLowerCase()) //.getAttribute("href") would get #color
-	})
-})
+
